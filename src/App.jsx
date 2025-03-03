f80d8fecdff2754f43e445b66b73ea2ad6e31d45
BrayanMontenegro <montenegrobrayab45@gmail.com>
2025-03-03 11:06:50 -0600

    pratica1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, Text } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -21,12 +21,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Text>Hola</Text>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Buenas tardes
       </p>
     </>
   )
